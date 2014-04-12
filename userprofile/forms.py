ce59a9be8366f127cc21a0317224f0f861eb78a1
Raghu Kalluri <raghu1kalluri@gmail.com>
2014-04-12 17:24:02 +0530

    profile editing form

--- userprofile/forms.py
+++ userprofile/forms.py
@@ -3,5 +3,28 @@ from userprofile.models import UserProfile
 
 class UserProfileForm(forms.ModelForm):
 
+	first_name = forms.CharField(max_length = 30)
+	last_name = forms.CharField(max_length = 30)
+
 	class Meta:
 		model = UserProfile
+		exclude = ('user',)
+		widgets = {
+			'description': forms.Textarea(attrs={'cols': 100, 'rows': 5})
+		}
+
+	def __init__(self, *args, **kwargs):
+		super(UserProfileForm, self).__init__(*args, **kwargs)
+		self.fields.keyOrder = ['first_name', 'last_name', 'ph_num', 'hostel', 'room', 'expertise', 
+			'social_media_URL', 'description', 'pic']
+
+
+"""	first_name = forms.CharField( max_length = 30)
+	last_name = forms.CharField( max_length = 30)
+	description = forms.CharField( max_length = 2500)
+	ph_num = forms.CharField( max_length = 13)
+	hostel = forms.CharField( max_length = 30)
+	room = forms.CharField( max_length = 4)
+	expertise = forms.CharField( max_length = 2500)
+	sm_url = forms.CharField( max_length = 500)
+	pic = forms.ImageField( upload_to = 'media/user_pics/')"""
--- userprofile/views.py
+++ userprofile/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from userprofile.forms import *
+from userprofile.models import *
 
-# Create your views here.
+def account(request):
+	user1 = UserProfile.objects.get(user__id = request.user.id)
+	cd1 = {'user': request.user}
+	return render(request, 'user/edit_account.html', cd1)
+
+def test(request):
+	return render(request, 'user/edit_account1.html', {'user': request.user})	
