79b494b2442156211ad4ed9a1dadeb574030732b
Raghu Kalluri <raghu1kalluri@gmail.com>
2014-04-14 15:58:48 +0530

    link base template to login,logout

--- userprofile/views.py
+++ userprofile/views.py
@@ -4,6 +4,9 @@ from userprofile.models import *
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, HttpResponseRedirect
 from userprofile.ajax import get_profile
+from project.models import Club
+
+club_list = Club.objects.all()
 
 @login_required
 def account(request):
@@ -11,8 +14,8 @@ def account(request):
 		user1 = UserProfile.objects.get(user = request.user)
 		form1 = UserProfileForm(instance = user1, initial = {'first_name': user1.user.first_name, 'last_name': user1.user.last_name})
 	except:
-		form1 = UserProfileForm(initial = {'first_name': request.user.first_name, 'last_name': request.user.last_name})
-	cd1 = {'form': form1, 'user': request.user}
+		form1 = UserProfileForm(initial = {'first_name': request.user.first_name, 'last_name': request.user.last_name})	
+	cd1 = {'form': form1, 'user': request.user, 'club_list': club_list}
 	return render(request, 'user/edit_account.html', cd1)
 
 def test(request):
@@ -27,7 +30,7 @@ def upload(request):
 			user1 = UserProfile(user = request.user)
 		form2 = UserProfileForm(request.POST, request.FILES, instance = user1)
 		if not form2.is_valid() or form2 is None:
-			cd2 = {'user': request.user, 'form': form2, 'decide': 1,}
+			cd2 = {'user': request.user, 'form': form2, 'decide': 1, 'club_list': club_list}
 			return render(request, 'user/edit_account.html', cd2)
 		else:
 			form2.save()
@@ -36,3 +39,5 @@ def upload(request):
 			u.last_name = form2.cleaned_data['last_name']
 			u.save()
 			return HttpResponseRedirect('/account/user/')
+	else:
+	 	HttpResponseRedirect('/account/user/')
